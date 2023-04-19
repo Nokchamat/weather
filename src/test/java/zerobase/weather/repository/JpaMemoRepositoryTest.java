cc74dd8bbcfdc69cd21ba6641e160626f3d12a64
Kang Sung-hyeok <rkdtjd976@gmail.com>
2023-04-19 16:45:25 +0900

    Initial commit

---
+++ src/test/java/zerobase/weather/repository/JpaMemoRepositoryTest.java
@@ -0,0 +1,49 @@
+package zerobase.weather.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class JpaMemoRepositoryTest {
+
+    @Autowired
+    JpaMemoRepository jpaMemoRepository;
+
+    @Test
+    void insertMemoTest(){
+        //given
+        Memo newMemo = new Memo(10, "this is jpa memo");
+
+        //when
+        jpaMemoRepository.save(newMemo);
+
+        //then
+        List<Memo> memoList = jpaMemoRepository.findAll();
+        assertTrue(memoList.size() > 0);
+    }
+
+    @Test
+    void findById(){
+        //given
+        Memo newMemo = new Memo(11,"Jpa");
+
+        //when
+        Memo save = jpaMemoRepository.save(newMemo);
+
+        //then
+        Optional<Memo> byId = jpaMemoRepository.findById(save.getId());
+
+        assertEquals(byId.get().getText(), "Jpa");
+
+    }
+
+}
